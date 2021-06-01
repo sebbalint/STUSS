830ceffcb32510e1f9c9af522dca9afe2894a45a
Nico <nicoletta.puetz@web.de>
2021-06-01 08:31:40 +0200

    First tries working

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
-#!/usr/bin/env pybricks-micropython
-
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-
-# Create your objects here
-
-brick = EV3Brick()
-
-# Write your program here
-
-
-# Beep. To get your attention.
-brick.sound.beep()
-
-# Clear the display
-brick.display.clear()
-
-# Print ``Hello`` near the middle of the screen
-brick.display.text("Hello", (60, 50))
-
-# Print ``World`` directly underneath it
-brick.display.text("World")
-
-# Show the current voltage
-brick.display.text("Voltage is: {}".format(brick.battery.voltage()))
-
-# Wait until a button is pressed
-while not brick.buttons():
-    wait(10)
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+import os
+os.system('setfont Lat15-TerminusBold14')
+mL = LargeMotor('outB'); mL.stop_action = 'hold'
+mR = MediumMotor('outC'); mR.stop_action = 'hold'
+print('Hello, my name is EV3!')
+Sound.speak('Hello, my name is EV3!').wait()
+mL.run_to_rel_pos(position_sp= 840, speed_sp = 250)
+mR.run_to_rel_pos(position_sp=-840, speed_sp = 250)
+mL.wait_while('running')
+mR.wait_while('running')
