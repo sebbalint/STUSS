db36e15d88491a606f2bd21a52056ae33db6036b
Sebastian Balint <S.Balint@web.de>
2021-06-08 11:28:08 +0200

    Alles liegt in Scherben und muss jetzt funktional gemacht werden :)

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
 import os
+
+from pybricks.hubs import EV3Brick
+from pybricks.parameters import Button
+
+#menu button functions
+from menu import wait_for_button
+from transit import auto_transit
+
+
+
+
+
+#setup und inits
+
+ev3 = EV3Brick()
 os.system('setfont Lat15-TerminusBold14')
-mL = LargeMotor('outB'); mL.stop_action = 'hold'
-mR = MediumMotor('outC'); mR.stop_action = 'hold'
+
+# Connect two large motors on output ports B and C
+lr_M = LargeMotor('outB'); mL.stop_action = 'hold'
+ud_M = MediumMotor('outC'); mR.stop_action = 'hold'
+
+# Turn leds off
+Leds.all_off()
+
+
+
+
+
+#program
 print('Hello, my name is EV3!')
 Sound.speak('Hello, my name is EV3!').wait()
-mL.run_to_rel_pos(position_sp= 840, speed_sp = 250)
-mR.run_to_rel_pos(position_sp=-840, speed_sp = 250)
+mL.run_to_rel_pos(position_sp = 840, speed_sp = 250)
+mR.run_to_rel_pos(position_sp = -840, speed_sp = 250)
 mL.wait_while('running')
 mR.wait_while('running')
+
+#While-Execute-Schleife (Hauptprogramm)
+while True:
+    # Show the menu and wait for one button to be selected.
+    button = wait_for_button(ev3)
+
+    # Now you can do something, based on which button was pressed.
+
+    # In this demo, we just play a different sound for each button.
+    if button == Button.LEFT:
+        ev3.speaker.beep(200)
+    elif button == Button.RIGHT:
+        ev3.speaker.beep(400)
+    elif button == Button.UP:
+        ev3.speaker.beep(600)
+    elif button == Button.DOWN:
+        ev3.speaker.beep(800)
+    elif button == Button.CENTER:
+        autotransit(ev3)
---
+++ transit.py
@@ -0,0 +1,34 @@
+from transit_util import util
+
+def auto_transit(ev3):
+    """
+    This function transitions the gondola from one side to the other.
+
+    Richtung hier als Parameter? also links nach rechts oder so TODO
+
+    Returns true on termination
+    """
+
+    # Show a picture of maybe "transition in progress" TODO
+    #ev3.screen.load_image('auto_transition.png')
+
+    # Tip: add text or icons to the image to help you
+    # remember what each button will do in your program.
+
+    
+    return true
+
+"""
+# Assign event handler to each of the remote buttons
+rc.on_red_up    = roll(lmotor, Leds.LEFT,   5)
+rc.on_red_down  = roll(lmotor, Leds.LEFT,  -5)
+rc.on_blue_up   = roll(rmotor, Leds.RIGHT,  5)
+rc.on_blue_down = roll(rmotor, Leds.RIGHT, -5)
+"""
+
+"""
+TODO functions:
+
+free_transit (TODO soll hier dann am Ende )
+calibrate (returns sind dann die Dimensionen)
+"""
